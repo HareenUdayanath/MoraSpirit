a235121d29e76c3905e94d0deef32159a9f16068
irfad0101 <irfad0101@gamil.com>
2016-01-14 18:36:09 +0530

    design user & equipment pages

--- resources/views/adminViews/adminResources.blade.php
+++ resources/views/adminViews/adminResources.blade.php
@@ -6,47 +6,355 @@
         <h3>Admin</h3>
         <ul class="nav side-menu">
             <li>
-                <a href={{route('adminHome')}}><i class="fa fa-home"></i> Home <!--span class="fa fa-chevron-down"></--span--></a>
+                <a href={{route('adminHome')}}><i class="fa fa-home"></i> Home </a>
             </li>
             <li>
-                <a> <i class="fa fa-users"></i> Users <span class="fa fa-chevron-down"></span> </a>
-                <ul class="nav child_menu" style="display: none">
-                    <li> <a href={{route('adminUsers')}}> Manage Users </a> </li>
-                    <li> <a href={{route('adminAddUser')}}> Add New User </a> </li>
-                </ul>
+                <a href={{route('adminUsers')}}> <i class="fa fa-users"></i> Users </a>
             </li>
             <li>
-                <a><i class="fa fa-soccer-ball-o"></i>Sports <span class="fa fa-chevron-down"></span> </a>
-                <ul class="nav child_menu" style="display: none">
-                    <li> <a href={{route('adminSports')}}> Manage Sports </a> </li>
-                    <li> <a href=#> Add New Sport </a> </li>
-                </ul>
+                <a href={{route('adminSports')}}><i class="fa fa-soccer-ball-o"></i> Sports </a>
             </li>
             <li>
-                <a><i class="fa fa-cubes"></i>Sport Equipments <span class="fa fa-chevron-down"></span> </a>
-                <ul class="nav child_menu" style="display: none">
-                    <li> <a href={{route('adminEquipments')}}> Manage Sports Equipments </a> </li>
-                    <li> <a href=#> Add New Equipment </a> </li>
-                </ul>
+                <a href={{route('adminEquipments')}}><i class="fa fa-cubes"></i> Sport Equipments </a>
             </li>
             <li>
-                <a><i class="fa fa-building-o"></i>Resources <span class="fa fa-chevron-down"></span> </a>
-                <ul class="nav child_menu" style="display: none">
-                    <li> <a> Manage Resources </a> </li>
-                    <li> <a href=#> Add New Resource </a> </li>
-                </ul>
+                <a><i class="fa fa-building-o"></i> Resources </a>
             </li>
             <li>
-                <a><i class="fa fa-child"></i>Students <span class="fa fa-chevron-down"></span> </a>
-                <ul class="nav child_menu" style="display: none">
-                    <li> <a href={{route('adminStudents')}}> Manage Students </a> </li>
-                    <li> <a href=#> Add New Student </a> </li>
-                </ul>
+                <a href={{route('adminStudents')}}><i class="fa fa-child"></i> Students </a>
             </li>
         </ul>
     </div>
 @endsection
 
 @section('content')
-    <h1>Admin Resources</h1>
+    <h3>Users</h3>
+    <div class="" role="tabpanel" data-example-id="togglable-tabs">
+        <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#tab_content1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">Manage Users</a>
+            </li>
+            <li role="presentation" class=""><a href="#tab_content2" role="tab" id="profile-tab" data-toggle="tab"  aria-expanded="false">Add New User</a>
+            </li>
+        </ul>
+        <div id="myTabContent" class="tab-content">
+            <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="home-tab">
+                <section class="content">
+                    <div class="row">
+                        <h3>Manage Users</h3>
+                        <div class="form-group">
+                            <label class=" col-md-1 col-sm-1 col-xs-1"> Search By: </label>
+                            <div class="col-md-2 col-sm-2 col-xs-2">
+                                <select class="form-control">
+                                    <option> ID </option>
+                                    <option> Name </option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-6 top_search">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search for...">
+                                    <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button">Go!</button>
+                                    </span>
+                            </div>
+                        </div>
+                        <div class="col-xs-12">
+                            <div class="table-responsive">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Contact Number</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($users as $usr)
+                                        <tr>
+                                            <td>{{$usr->ID}}</td>
+                                            <td>{{$usr->Name}}</td>
+                                            <td>{{$usr->ContactNo}}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="profile-tab">
+                <div class="clearfix"></div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="x_panel">
+                            <div class="x_content">
+                                <br />
+                                <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="user-id"> ID <span class="required">*</span>
+                                        </label>
+                                        <div class="col-md-6 col-sm-6 col-xs-12">
+                                            <input type="text" id="user-id" required="required" class="form-control col-md-7 col-xs-12">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="user-name"> Name <span class="required">*</span>
+                                        </label>
+                                        <div class="col-md-6 col-sm-6 col-xs-12">
+                                            <input type="text" id="user-name" name="user-name" required="required" class="form-control col-md-7 col-xs-12">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="contact-num"> Contact Number <span class="required">*</span>
+                                        </label>
+                                        <div class="col-md-6 col-sm-6 col-xs-12">
+                                            <input type="text" id="contact-num" required="required" class="form-control col-md-7 col-xs-12">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12"> Role <span class="required">*</span>
+                                        </label>
+                                        <div class="col-md-6 col-sm-6 col-xs-12">
+                                            <select class="form-control">
+                                                <option> Coach </option>
+                                                <option> Keeper </option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="form-group">
+                                            <label class="control-label col-md-3 col-sm-3 col-xs-12"> Sport/Resource </label>
+                                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                                <select class="form-control">
+                                                    <option>Choose option</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="ln_solid"></div>
+                                    <div class="form-group">
+                                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                            <button type="submit" class="btn btn-primary">Cancel</button>
+                                            <button type="submit" class="btn btn-success">Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <script type="text/javascript">
+                    $(document).ready(function () {
+                        $('#birthday').daterangepicker({
+                            singleDatePicker: true,
+                            calender_style: "picker_4"
+                        }, function (start, end, label) {
+                            console.log(start.toISOString(), end.toISOString(), label);
+                        });
+                    });
+                </script>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('requiredJS')
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- chart js -->
+    <script src="js/chartjs/chart.min.js"></script>
+    <!-- bootstrap progress js -->
+    <script src="js/progressbar/bootstrap-progressbar.min.js"></script>
+    <script src="js/nicescroll/jquery.nicescroll.min.js"></script>
+    <!-- icheck -->
+    <script src="js/icheck/icheck.min.js"></script>
+    <!-- tags -->
+    <script src="js/tags/jquery.tagsinput.min.js"></script>
+    <!-- switchery -->
+    <script src="js/switchery/switchery.min.js"></script>
+    <!-- daterangepicker -->
+    <script type="text/javascript" src="js/moment.min2.js"></script>
+    <script type="text/javascript" src="js/datepicker/daterangepicker.js"></script>
+    <!-- richtext editor -->
+    <script src="js/editor/bootstrap-wysiwyg.js"></script>
+    <script src="js/editor/external/jquery.hotkeys.js"></script>
+    <script src="js/editor/external/google-code-prettify/prettify.js"></script>
+    <!-- select2 -->
+    <script src="js/select/select2.full.js"></script>
+    <!-- form validation -->
+    <script type="text/javascript" src="js/parsley/parsley.min.js"></script>
+    <!-- textarea resize -->
+    <script src="js/textarea/autosize.min.js"></script>
+    <script>
+        autosize($('.resizable_textarea'));
+    </script>
+    <!-- Autocomplete -->
+    <script type="text/javascript" src="js/autocomplete/countries.js"></script>
+    <script src="js/autocomplete/jquery.autocomplete.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            'use strict';
+            var countriesArray = $.map(countries, function (value, key) {
+                return {
+                    value: value,
+                    data: key
+                };
+            });
+            // Initialize autocomplete with custom appendTo:
+            $('#autocomplete-custom-append').autocomplete({
+                lookup: countriesArray,
+                appendTo: '#autocomplete-container'
+            });
+        });
+    </script>
+    <script src="js/custom.js"></script>
+
+
+    <!-- select2 -->
+    <script>
+        $(document).ready(function () {
+            $(".select2_single").select2({
+                placeholder: "Select a state",
+                allowClear: true
+            });
+            $(".select2_group").select2({});
+            $(".select2_multiple").select2({
+                maximumSelectionLength: 4,
+                placeholder: "With Max Selection limit 4",
+                allowClear: true
+            });
+        });
+    </script>
+    <!-- /select2 -->
+    <!-- input tags -->
+    <script>
+        function onAddTag(tag) {
+            alert("Added a tag: " + tag);
+        }
+
+        function onRemoveTag(tag) {
+            alert("Removed a tag: " + tag);
+        }
+
+        function onChangeTag(input, tag) {
+            alert("Changed a tag: " + tag);
+        }
+
+        $(function () {
+            $('#tags_1').tagsInput({
+                width: 'auto'
+            });
+        });
+    </script>
+    <!-- /input tags -->
+    <!-- form validation -->
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $.listen('parsley:field:validate', function () {
+                validateFront();
+            });
+            $('#demo-form .btn').on('click', function () {
+                $('#demo-form').parsley().validate();
+                validateFront();
+            });
+            var validateFront = function () {
+                if (true === $('#demo-form').parsley().isValid()) {
+                    $('.bs-callout-info').removeClass('hidden');
+                    $('.bs-callout-warning').addClass('hidden');
+                } else {
+                    $('.bs-callout-info').addClass('hidden');
+                    $('.bs-callout-warning').removeClass('hidden');
+                }
+            };
+        });
+
+        $(document).ready(function () {
+            $.listen('parsley:field:validate', function () {
+                validateFront();
+            });
+            $('#demo-form2 .btn').on('click', function () {
+                $('#demo-form2').parsley().validate();
+                validateFront();
+            });
+            var validateFront = function () {
+                if (true === $('#demo-form2').parsley().isValid()) {
+                    $('.bs-callout-info').removeClass('hidden');
+                    $('.bs-callout-warning').addClass('hidden');
+                } else {
+                    $('.bs-callout-info').addClass('hidden');
+                    $('.bs-callout-warning').removeClass('hidden');
+                }
+            };
+        });
+        try {
+            hljs.initHighlightingOnLoad();
+        } catch (err) {}
+    </script>
+    <!-- /form validation -->
+    <!-- editor -->
+    <script>
+        $(document).ready(function () {
+            $('.xcxc').click(function () {
+                $('#descr').val($('#editor').html());
+            });
+        });
+
+        $(function () {
+            function initToolbarBootstrapBindings() {
+                var fonts = ['Serif', 'Sans', 'Arial', 'Arial Black', 'Courier',
+                            'Courier New', 'Comic Sans MS', 'Helvetica', 'Impact', 'Lucida Grande', 'Lucida Sans', 'Tahoma', 'Times',
+                            'Times New Roman', 'Verdana'],
+                        fontTarget = $('[title=Font]').siblings('.dropdown-menu');
+                $.each(fonts, function (idx, fontName) {
+                    fontTarget.append($('<li><a data-edit="fontName ' + fontName + '" style="font-family:\'' + fontName + '\'">' + fontName + '</a></li>'));
+                });
+                $('a[title]').tooltip({
+                    container: 'body'
+                });
+                $('.dropdown-menu input').click(function () {
+                            return false;
+                        })
+                        .change(function () {
+                            $(this).parent('.dropdown-menu').siblings('.dropdown-toggle').dropdown('toggle');
+                        })
+                        .keydown('esc', function () {
+                            this.value = '';
+                            $(this).change();
+                        });
+
+                $('[data-role=magic-overlay]').each(function () {
+                    var overlay = $(this),
+                            target = $(overlay.data('target'));
+                    overlay.css('opacity', 0).css('position', 'absolute').offset(target.offset()).width(target.outerWidth()).height(target.outerHeight());
+                });
+                if ("onwebkitspeechchange" in document.createElement("input")) {
+                    var editorOffset = $('#editor').offset();
+                    $('#voiceBtn').css('position', 'absolute').offset({
+                        top: editorOffset.top,
+                        left: editorOffset.left + $('#editor').innerWidth() - 35
+                    });
+                } else {
+                    $('#voiceBtn').hide();
+                }
+            };
+
+            function showErrorAlert(reason, detail) {
+                var msg = '';
+                if (reason === 'unsupported-file-type') {
+                    msg = "Unsupported format " + detail;
+                } else {
+                    console.log("error uploading file", reason, detail);
+                }
+                $('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                        '<strong>File upload error</strong> ' + msg + ' </div>').prependTo('#alerts');
+            };
+            initToolbarBootstrapBindings();
+            $('#editor').wysiwyg({
+                fileUploadError: showErrorAlert
+            });
+            window.prettyPrint && prettyPrint();
+        });
+    </script>
 @endsection
